f999be8644dab669f1f800897b60ef8b19e08b24
alejandropch <alejandropachas1@gmail.com>
2021-07-29 22:06:20 -0500

    NavBar style update, unnecessaries css files were remove

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-
 export default function NavBar() {
     return (
             <section className="Header">
-            <div className="Header__container">
+                <div className="Header__container">
 
-        <ul>
-            <li className="Header__container">About</li>
-            <li className="Header__container">Projects</li>
-            <li className="Header__container">Contact</li>
+                 <ul className="Header__container--list">
+                    <li className="list item">About</li>
+                    <li className="list item">Projects</li>
+                    <li className="list item">Contact</li>
 
-        </ul>
+                 </ul>
 
 
             </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import '../styles/MainPage.css'
+
 export default function MainPage() {
     return (
         <React.Fragment>
