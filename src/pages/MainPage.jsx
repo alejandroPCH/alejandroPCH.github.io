6dfebe63697c70215c74cc9d8c8959132863b7b9
alejandropch <alejandropachas1@gmail.com>
2021-07-30 21:33:06 -0500

    backgoud gradient attachment fixed

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,12 +4,16 @@ import React from 'react'
 export default function MainPage() {
     return (
         <React.Fragment>
-        <section className="hi">
+        <section className="hi gradient">
             <div className="hi__container">
               
               <h1 className="hi__container--text">Hi</h1>
-              <a href="#" className="hi__container--arrow-down">abajo</a>
+              <a href="#" className="hi__container--arrow-down">arrow</a>
 
+            </div>
+        </section>
+        <section className="profile gradient">
+            <div className="profile__container">
             </div>
         </section>
         </React.Fragment>
