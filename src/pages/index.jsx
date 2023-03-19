62eaa702ffa7f23fcc9be745c2ca2fc2f84ffc54
Alejandro Pachas <alejandromydev@gmail.com>
2023-03-19 14:02:32 -0500

    removing unwanted imports,  indenting file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,18 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import Piano from '@/components/Piano'
-
-/* import DynamicImport from '@/components/DynamicImport' */
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Piano from "@/components/Piano";
 
 export default function Home() {
-
-
   return (
     <>
       <Head>
         <title>Alejandro Pachas</title>
         <meta name="description" content="Alejandro's web page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/*<link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <main className='bg-white'>
-        <p className='text-black bold text-4xl'>hi</p>
+      <main className="bg-white">
+        <p className="text-black bold text-4xl">hi</p>
         <Piano />
       </main>
     </>
-  )
+  );
 }
