c31982d54e2485a4aaf983731973ec6306e3853a
Alejandro Pachas <alejandromydev@gmail.com>
2023-03-19 14:07:57 -0500

    folder 'utils' added, with Piano.js classadded

---
+++ src/utils/Piano.js
@@ -0,0 +1,99 @@
+import * as Tone from "tone";
+
+export default class Piano {
+  keys = [];
+  synth = {};
+
+  constructor() {
+    this.keys = [
+      new Key("KeyA", "C4"),
+      new Key("KeyS", "D4"),
+      new Key("KeyD", "E4"),
+      new Key("KeyF", "F4"),
+      new Key("KeyG", "G4"),
+      new Key("KeyH", "A4"),
+      new Key("KeyJ", "B4"),
+      new Key("KeyK", "C5"),
+      new Key("KeyL", "D5"),
+    ];
+  }
+
+  get synth() {
+    return this.synth;
+  }
+
+  make() {
+    async () => await Tone.start();
+
+    this.synth = new Tone.Sampler({
+      urls: {
+        C4: "C4.mp3",
+        "D#4": "Ds4.mp3",
+        "F#4": "Fs4.mp3",
+        A4: "A4.mp3",
+      },
+
+      baseUrl: "https://tonejs.github.io/audio/salamander/",
+      release: 10,
+
+      onload: () => {
+        synth.triggerAttackRelease(["C1", "E1", "G1", "B1"], 0.5);
+      },
+    }).toDestination();
+  }
+
+  async getKey(event_code) {
+    return new Promise((resolve, reject) => {
+      this.keys.map((key) => {
+        if (event_code == key.event_code) {
+          resolve(key);
+        }
+      });
+    });
+  }
+  async playNote(key) {
+    if (key.is_pressed == true) return;
+    key.pressed = true;
+    await Tone.start();
+
+    return Tone.loaded().then(() => {
+      this.synth.triggerAttack(key.note);
+    });
+  }
+
+  async releaseNote(key) {
+    key.pressed = false;
+    await Tone.start();
+
+    return Tone.loaded().then(() => {
+      this.synth.triggerRelease(key.note);
+    });
+  }
+}
+
+class Key {
+  event_code = "";
+  note = "";
+  pressed = false;
+
+  constructor(event_code, note) {
+    this.event_code = event_code;
+    this.note = note;
+  }
+
+  get event_code() {
+    return this.event_code;
+  }
+
+  get is_pressed() {
+    return this.pressed;
+  }
+
+  get note() {
+    return this.note;
+  }
+
+  set pressed(pressed) {
+    this.pressed = pressed;
+  }
+}
