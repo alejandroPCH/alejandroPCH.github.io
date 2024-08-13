766279d52b865353ed0d5c55a9a010b3d87602a9
Alejandro Pachas <alejandropachas1@gmail.com>
2024-08-12 19:51:18 -0500

    [src/components/Piano.jsx] component updated, accidental & natural key component now being used

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState, useRef } from "react"
 import PianoInstrument from "@/utils/Piano"
-import { Key } from "./Key"
+import { NaturalKey } from "./NaturalKey"
+import { AccidentalKey } from "./AccidentalKey"
 
 export default function Piano() {
-  const [piano, setPiano] = useState(new PianoInstrument())
+  const [piano] = useState(new PianoInstrument())
   const [key_pressed, setKeyPressed] = useState("")
   const [key_released, setKeyReleased] = useState("")
 
   useEffect(() => {
     piano.make()
-  }, [])
+  }, [piano])
 
   useEffect(() => {
     if (!piano.synth) return
@@ -35,21 +36,42 @@ export default function Piano() {
     piano.getKey(e.code).then(async (key) => {
       setKeyPressed(key)
       setKeyReleased(key)
+
       await piano.releaseNote(key)
     })
   }
-
+  
   return (
-    <div className="w-full flex justify-center items-center h-96 relative">
-      {piano.keys.map((key, index) => (
-        <Key
-          key={key.event_code}
-          index={index}
-          props={key}
-          key_pressed={key_pressed}
-          key_released={key_released}
-        />
-      ))}
+    <div className="h-screen flex justify-center items-center w-full z-10">
+      <div className="grid grid-rows-2 grid-cols-1 h-full max-h-[30rem] relative">
+      <div className="grid grid-cols-9 row-start-1 grid-rows-2 col-start-1 row-span-2">
+        {piano.keys.map((key, index) => {
+          
+          return (
+            !key.accidental && // if key.accidental is false, then is a Natural Key
+              <NaturalKey
+                key={key.event_code}
+                props={[key, key_pressed, key_released, index]}
+              />
+          )
+        })}
+        </div>
+        <div className="flex flex-row row-start-1 row-1 col-start-1 ml-[3.4rem]">
+        {piano.keys.map((key) => {
+            let extra_spacing = (key.note.includes("D#") || key.note.includes("A#")) && "mr-[7rem]"  // if D# or A# key
+
+            return (
+              key.accidental &&
+                <AccidentalKey 
+                  key={key.event_code}
+                  props={[key, key_pressed, key_released]}
+                  className={"mr-[1rem] "+ extra_spacing}
+                />
+            )
+          })
+        }
+        </div>
+      </div>
     </div>
   )
 }
