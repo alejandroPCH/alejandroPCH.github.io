815969596b98316d5e9abe146385353b7f76349b
Alejandro Pachas <alejandropachas1@gmail.com>
2024-08-16 17:32:43 -0500

    [index.js] using components created, background shodow effect added for smooth transition

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,13 @@
 import Head from "next/head"
-import Piano from "@/components/Piano"
-import Hero from "@/components/Hero"
+import PianoContainer from "@/components/Piano/PianoContainer"
+import HeroContainer from "@/components/Hero/HeroContainer"
+import dynamic from "next/dynamic"
+import ContactContainer from "@/components/Contact/ContactContainer"
+
+const SidebarContainer = dynamic(()=> import("@/components/Sidebar/SidebarContainer"), {ssr:false})
 
 export default function Home({role}) {
+  
 
   return (
     <>
@@ -12,8 +17,11 @@ export default function Home({role}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <Piano />
-        <Hero />
+        <SidebarContainer />
+        <PianoContainer />
+        <HeroContainer />
+        <div  style={{background:"linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%"}}  className="absolute bg-white w-screen h-16 z-30 mt-[-4rem]" ></div> 
+        <ContactContainer />
       </main>
     </>
   )
